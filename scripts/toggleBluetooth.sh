f0ba20fe7f778bfe8cd4cec883b20954d48c8303
JoshVanL <jv15626@my.bristol.ac.uk>
2017-09-27 12:33:33 +0100

    Moving Wallpapers

--- scripts/toggleBluetooth.sh
+++ scripts/toggleBluetooth.sh
@@ -5,7 +5,9 @@ service=bluetooth.service
 if [[ $(systemctl is-active $service) == "active" ]]
 then
     systemctl stop bluetooth.service
+    rfkill block 0
 else
+    rfkill unblock 0
     systemctl start bluetooth.service
 fi
 
